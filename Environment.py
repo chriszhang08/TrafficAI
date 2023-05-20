95017b4f0089e845b9004be404fcd5d3f6a8a1c4
chrzhang <chrzhang@umich.edu>
2023-05-20 19:51:32 -0400

    super simple braking

--- Environment.py
+++ Environment.py
@@ -9,7 +9,7 @@ import random
 pygame.init()
 
 # Set up the display
-width, height = 800, 600
+width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Highway Simulation")
 
@@ -22,12 +22,12 @@ yellow = (255, 255, 0)
 red = (255, 0, 0)
 
 # Number of lanes
-num_lanes = 3
+num_lanes = 1
 
 # Highway properties
 lane_width = 50
 all_lanes_width = lane_width * num_lanes
-lane_height = 600
+lane_height = height
 lane_x = (width - all_lanes_width) // 2
 lane_y = height - lane_height
 
@@ -40,37 +40,61 @@ num_lines = lane_height // (line_height + line_gap)
 # Car properties
 car_width = 15
 
+
 # Car class
-class Car:
+class Car(pygame.sprite.Sprite):
     def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
         # Randomly choose a lane
         self.x = x
         self.y = 0
         self.color = red
         self.car_radius = 15
-        self.car_speed = random.randint(1, 5)
+        self.car_speed = random.randint(4, 5)
+        # TODO implement car state (accelerating, braking, stopped, etc.)
+        self.state = "cruising"
 
     def move(self):
         # TODO implement variable car speeds depending on conditions
+        if (self.state == "braking"):
+            self.car_speed -= .2
+            if (self.car_speed <= 0):
+                self.car_speed = 0
+                self.state = "stopped"
+        elif (self.state == "stopped"):
+            self.car_speed = 0
+        elif (self.state == "cruising"):
+            self.car_speed = 5
         self.y += self.car_speed
         if self.y > height:
             self.car_radius = 0
 
     def brake(self):
         # gradually slow down
-        while self.car_speed > 0:
-            self.car_speed -= 1
+        self.state = "braking"
 
     def draw(self):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.car_radius)
 
+
 # Create lanes
 lanes = []
 for i in range(num_lanes):
-    lane_cars = []
+    lane_cars = pygame.sprite.Group()
     lanes.append(lane_cars)
 
-# TODO create a function that takes an array of cars and returns how many cars are stopping
+
+# TODO create a function that takes a pygame Group of Cars and changes their states so that
+# they accelerate if there is space in front of them, brake if they are too close to the car in front of them,
+# and stop if they are at the end of the highway
+def update_car_states(cars):
+    list_of_cars = cars.sprites()
+    for i in range(1, len(list_of_cars)):
+        if ((list_of_cars[i-1].y - list_of_cars[i].y < 100) and
+                (list_of_cars[i-1].state == "braking" or list_of_cars[i-1].state == "stopped")):
+            list_of_cars[i].brake()
+
+
 # TODO create a function that allows cars to change lanes
 
 # Game loop
@@ -101,15 +125,18 @@ while running:
             car_x = lane_x + (lane_width // 2) * (2 * i + 1)
             car_y = height
             new_car = Car(car_x, car_y)
-            lanes[i].append(new_car)
+            lanes[i].add(new_car)
 
     # Move and draw cars in each lane
     for lane_cars in lanes:
         for car in lane_cars:
+            update_car_states(lane_cars)
             car.move()
             car.draw()
             if car.y > height:
-                lane_cars.remove(car)
+                car.kill()
+            if car.y > height - 400 and car.car_speed > 0:
+                car.brake()
 
     pygame.display.update()
     clock.tick(60)
